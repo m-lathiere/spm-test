2d4dfb3fdb0d66081415ac5163d929116d9e6cc4
Maxime Lathiere <m.lathiere@kreactive.com>
2024-02-14 08:41:30 +0100

    Initial Commit

---
+++ Tests/SPM-TestTests/SPM_TestTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import SPM_Test
+
+final class SPM_TestTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
